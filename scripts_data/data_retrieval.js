ada8ead24150e9e6f21286e1e436c9032f17f851
Quentin Vincenot <quentin.vincenot@gmail.com>
2024-02-04 19:51:57 +0100

    Generated random data for line chart

--- scripts_data/data_retrieval.js
+++ scripts_data/data_retrieval.js
@@ -2,44 +2,57 @@
 ----- RETRIEVED DATA INFORMATION ABOUT CITIES METEO
 --------------------------------------------------------------------------------*/
 
-document.getElementById('myChart').style.width = '580px';
-document.getElementById('myChart').style.height = '280px';
-const ctx = document.getElementById('myChart').getContext('2d');
+document.getElementById('barchart_canvas').style.width = '700px';
+document.getElementById('barchart_canvas').style.height = '250px';
+const ctx = document.getElementById('barchart_canvas').getContext('2d');
 
 new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    responsive: true
-  }
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3, 12, 15, 10, 7, 9, 5],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        responsive: true, layout: {padding: {top: 5, left: 10, right: 10, bottom: 5}}
+    }
 });
 
-document.getElementById('lineChart').style.width = '580px';
-document.getElementById('lineChart').style.height = '280px';
-const ctx2 = document.getElementById('lineChart').getContext('2d');
+
+
+let LINE_DATAPOINTS = [], LINE_DATALABELS = [];
+for(let i=0; i<100; i++) {
+    LINE_DATAPOINTS.push(Math.random() * 25 - 5);
+    LINE_DATALABELS.push(''+(i+1));
+}
+console.log(LINE_DATAPOINTS);
+console.log(LINE_DATALABELS);
+
+
+document.getElementById('linechart_canvas').style.width = '1200px';
+document.getElementById('linechart_canvas').style.height = '300px';
+const ctx2 = document.getElementById('linechart_canvas').getContext('2d');
 
 new Chart(ctx2, {
-  type: 'line',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      responsive: true
+    type: 'line',
+    data: {
+        labels: LINE_DATALABELS,
+        datasets: [{
+            label: '# of Votes',
+            data: LINE_DATAPOINTS,
+            borderWidth: 2
+        }]
+    },
+    options: {
+        responsive: true, layout: {padding: {top: 5, left: 10, right: 10, bottom: 5}},
+        elements: {
+            point: {radius: 2}
+        }
     }
-  }
 });
 
 
