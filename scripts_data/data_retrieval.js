b60be756f5832ae3c28711f701041d2b269e4e18
Quentin Vincenot <quentin.vincenot@gmail.com>
2024-02-05 22:05:58 +0100

    Initialized the line chart at page loading with retrieved data from the API

--- scripts_data/data_retrieval.js
+++ scripts_data/data_retrieval.js
@@ -19,84 +19,89 @@ function get_and_display_today_date() {
 get_and_display_today_date();
 
 
+
+// Function to initialize the bar chart
+function initialize_bar_chart() {
+    document.getElementById('barchart_canvas').style.width = '700px';
+    document.getElementById('barchart_canvas').style.height = '250px';
+    const ctx = document.getElementById('barchart_canvas').getContext('2d');
+    
+    bar_chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June',
+                'July', 'August', 'September', 'October', 'November', 'December'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3, 12, 15, 10, 7, 9, 5],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            responsive: true, layout: {padding: {top: 5, left: 10, right: 10, bottom: 5}}
+        }
+    });
+}
+let bar_chart = undefined;
+initialize_bar_chart();
+
+
+
+// Function to retrieve and store meteo data from the open-meteo online API
+async function retrieve_meteo_data() {
+    // Create and trigger the request to get data from the open-meteo online API
+    let request_url = "https://api.open-meteo.com/v1/meteofrance?latitude=52.52&longitude=13.41&" +
+                        "daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin&" +
+                        "start_date=2023-11-01&end_date=2024-01-31";
+    let response = await fetch(request_url);
+    let data = await response.json();
+
+    // Affect initially retrieved datapoints to the line chart data
+    LINE_DATALABELS = data.daily.time;
+    LINE_DATAPOINTS_MIN = data.daily.temperature_2m_min;
+    LINE_DATAPOINTS_MAX = data.daily.temperature_2m_max;
+}
+
+// Function to initialize the line chart containing the latest 90 values of a measurement
+async function initialize_line_chart() {
+    // Configure the dimensions of the canvas containing the latest measurements line chart
+    document.getElementById('linechart_canvas').style.width = '1200px';
+    document.getElementById('linechart_canvas').style.height = '300px';
+    
+    // Request the online API to retrieve data to display in the line chart
+    await retrieve_meteo_data();
+    
+    // Retrieve the context of the canvas used to display the chart with Chart.js
+    const ctx2 = document.getElementById('linechart_canvas').getContext('2d');
+    // Create the line chart with retrieved data from the online API
+    line_chart = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: LINE_DATALABELS,
+            datasets: [
+                { label: 'Min Temperature (°C)', data: LINE_DATAPOINTS_MIN,
+                    backgroundColor: 'rgba(54, 162, 235, 0.6)', borderWidth: 2 },
+                { label: 'Max Temperature (°C)', data: LINE_DATAPOINTS_MAX,
+                    backgroundColor: 'rgba(255, 159, 64, 0.6)', borderWidth: 2 },
+            ]
+        },
+        options: {
+            responsive: true, layout: {padding: {top: 5, left: 15, right: 15, bottom: 10}},
+            elements: {point: {radius: 2}}
+        }
+    });    
+}
+// Save references to the line chart, line data labels and line data points for later reuse/update
+let line_chart = undefined, LINE_DATALABELS = [], LINE_DATAPOINTS_MIN = [], LINE_DATAPOINTS_MAX = [];
+// Retrieve data from the open-meteo API and initialize the line chart with the retrieved data points
+initialize_line_chart();
+
+
+
 let icon_temperature = "<i class='fa-solid fa-temperature-half' style='font-size:50px;'></i>";
 let icon_humidity = "<i class='fa-solid fa-droplet' style='font-size:50px;'></i>";
 let icon_precipitation = "<i class='fa-solid fa-cloud-showers-heavy' style='font-size:50px;'></i>";
 let icon_pressure = "<i class='fa-solid fa-gauge-high' style='font-size:50px;'></i>";
 let icon_wind = "<i class='fa-solid fa-wind' style='font-size:50px;'></i>";
-
-
-document.getElementById('barchart_canvas').style.width = '700px';
-document.getElementById('barchart_canvas').style.height = '250px';
-const ctx = document.getElementById('barchart_canvas').getContext('2d');
-
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June',
-            'July', 'August', 'September', 'October', 'November', 'December'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3, 12, 15, 10, 7, 9, 5],
-            borderWidth: 2
-        }]
-    },
-    options: {
-        responsive: true, layout: {padding: {top: 5, left: 10, right: 10, bottom: 5}}
-    }
-});
-
-
-
-let LINE_DATAPOINTS = [], LINE_DATALABELS = [];
-for(let i=0; i<100; i++) {
-    LINE_DATAPOINTS.push(Math.random() * 25 - 5);
-    LINE_DATALABELS.push(''+(i+1));
-}
-console.log(LINE_DATAPOINTS);
-console.log(LINE_DATALABELS);
-
-
-document.getElementById('linechart_canvas').style.width = '1200px';
-document.getElementById('linechart_canvas').style.height = '300px';
-const ctx2 = document.getElementById('linechart_canvas').getContext('2d');
-
-new Chart(ctx2, {
-    type: 'line',
-    data: {
-        labels: LINE_DATALABELS,
-        datasets: [{
-            label: '# of Votes',
-            data: LINE_DATAPOINTS,
-            borderWidth: 2
-        }]
-    },
-    options: {
-        responsive: true, layout: {padding: {top: 5, left: 10, right: 10, bottom: 5}},
-        elements: {
-            point: {radius: 2}
-        }
-    }
-});
-
-
-
-
-//
-async function retrieve_meteo_data() {
-    let request_url = "https://api.open-meteo.com/v1/meteofrance?latitude=43.59&longitude=1.43&hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure,wind_speed_10m&timezone=Europe%2FBerlin&start_date=2024-01-01&end_date=2024-01-31";
-    let response = await fetch(request_url);
-    let data = await response.json();
-    console.log(data);
-}
-
-
-//
-retrieve_meteo_data();
-
-
-
-
-
 
 
